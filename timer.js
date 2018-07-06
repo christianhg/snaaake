535fb36e6adf8d34311a2dd3ad0802bf9addcb1b
Christian Hamburger Grøngaard <christianhg@gmail.com>
2018-07-06 23:19:59 +0200

    Rename accumulator to accumulatedTime

--- timer.js
+++ timer.js
@@ -1,14 +1,15 @@
 export const createTimer = ({ step, update, render, initialScene }) => () => {
   let lastTime = 0
-  let accumulator = 0
+  let accumulatedTime = 0
   let frameId
   let scene = initialScene
 
   const animate = time => {
     if (lastTime) {
-      accumulator = accumulator + (time - lastTime) / 1000
-      while (accumulator >= step) {
-        accumulator = accumulator - step
+      accumulatedTime = accumulatedTime + (time - lastTime) / 1000
+
+      while (accumulatedTime >= step) {
+        accumulatedTime = accumulatedTime - step
 
         scene = update(step, scene)
       }
