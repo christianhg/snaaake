85355449caea9c2270342b6a48a7bb1a3f68794d
Christian Hamburger Grøngaard <christianhg@gmail.com>
2018-07-08 19:23:57 +0200

    Make it possible to stop the game

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import 'modern-normalize'
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { Machine } from 'xstate'
 import { Canvas } from './canvas'
 import { Vec } from './math'
 import {
@@ -18,11 +19,44 @@ const update = (step, { circle, bounds }) => {
   }
 }
 
+const gameMachine = Machine({
+  initial: 'idle',
+  states: {
+    idle: {
+      on: {
+        START: 'running',
+      },
+    },
+    running: {
+      on: {
+        STOP: 'stopped',
+      },
+      onEntry: 'startGame',
+    },
+    stopped: {
+      on: {
+        START: 'running',
+      },
+      onEntry: 'stopGame',
+    },
+  },
+})
+
 class Froke extends Component {
   constructor() {
     super()
 
+    this.actionMap = {
+      startGame: () => {
+        this.stopTimer = this.startTimer()
+      },
+      stopGame: () => {
+        this.stopTimer()
+      },
+    }
+
     this.state = {
+      game: gameMachine.initialState.value,
       scene: {
         circle: createCircle(20, Vec(320, 320), Vec(100, 50)),
         bounds: createSquare(Vec(0, 0), Vec(640, 640)),
@@ -37,10 +71,40 @@ class Froke extends Component {
     })
   }
 
+  dispatch(event) {
+    const nextState = gameMachine.transition(this.state.game, event)
+
+    nextState.actions.forEach(actionKey => {
+      const action = this.actionMap[actionKey]
+
+      if (action) {
+        action(event)
+      }
+    })
+
+    this.setState({
+      game: nextState.value,
+    })
+  }
+
   render() {
     return (
       <div>
-        <button onClick={() => this.startTimer()}>Start</button>
+        <p>
+          Current state: <b>{this.state.game}</b>
+        </p>
+        {this.state.game === 'idle' || this.state.game === 'stopped' ? (
+          <button onClick={() => this.dispatch({ type: 'START' })}>
+            Start
+          </button>
+        ) : (
+          undefined
+        )}
+        {this.state.game === 'running' ? (
+          <button onClick={() => this.dispatch({ type: 'STOP' })}>Stop</button>
+        ) : (
+          undefined
+        )}
         <Canvas scene={this.state.scene} />
       </div>
     )
