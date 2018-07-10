cf8a72083d7c177e2fad54143b58bf7ac1365fed
Christian Hamburger Grøngaard <christianhg@gmail.com>
2018-07-10 22:14:52 +0200

    Replace timer with more powerful engine

---
+++ src/engine.js
@@ -0,0 +1,116 @@
+import { Machine } from 'xstate'
+import { bindKeys } from './keyboard'
+
+const engineMachine = Machine({
+  initial: 'idle',
+  states: {
+    idle: {
+      on: {
+        START: 'running',
+      },
+    },
+    running: {
+      on: {
+        PAUSE: 'paused',
+        STOP: 'stopped',
+      },
+      onEntry: 'startEngine',
+    },
+    paused: {
+      on: {
+        RESUME: 'running',
+        STOP: 'stopped',
+      },
+      onEntry: 'pauseEngine',
+    },
+    stopped: {
+      on: {
+        START: 'running',
+      },
+      onEntry: 'stopEngine',
+    },
+  },
+})
+
+export const createEngine = ({
+  step,
+  tick,
+  subscribe,
+  initialState,
+  keyBindings,
+}) => {
+  let status = engineMachine.initialState.value
+  let state = initialState
+  let lastTime = 0
+  let accumulatedTime = 0
+  let frameId
+  let unbind
+
+  const animate = time => {
+    if (lastTime) {
+      accumulatedTime = accumulatedTime + (time - lastTime) / 1000
+
+      while (accumulatedTime >= step) {
+        accumulatedTime = accumulatedTime - step
+
+        state = tick(step, state)
+      }
+    }
+
+    subscribe({ status, state })
+
+    lastTime = time
+    frameId = requestAnimationFrame(animate)
+  }
+
+  const actionMap = {
+    startEngine: () => {
+      frameId = requestAnimationFrame(animate)
+      unbind = bindKeys(keyBindings.element, keyBindings.bindings)
+    },
+    pauseEngine: () => {
+      lastTime = 0
+      cancelAnimationFrame(frameId)
+      unbind()
+    },
+    stopEngine: () => {
+      lastTime = 0
+      state = initialState
+      cancelAnimationFrame(frameId)
+      unbind()
+    },
+  }
+
+  const dispatch = event => {
+    const nextStatus = engineMachine.transition(status, event)
+
+    nextStatus.actions.forEach(actionKey => {
+      const action = actionMap[actionKey]
+
+      if (action) {
+        action(event)
+      }
+    })
+
+    status = nextStatus.value
+
+    subscribe({ status, state })
+  }
+
+  return {
+    getState: () => state,
+    getStatus: () => status,
+    start() {
+      dispatch({ type: 'START' })
+    },
+    pause() {
+      dispatch({ type: 'PAUSE' })
+    },
+    resume() {
+      dispatch({ type: 'RESUME' })
+    },
+    stop() {
+      dispatch({ type: 'STOP' })
+    },
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import 'modern-normalize'
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { Machine } from 'xstate'
 import { Canvas } from './canvas'
-import { bindKeys } from './keyboard'
+import { createEngine } from './engine'
 import { Vec } from './math'
 import {
   createCircle,
@@ -11,7 +10,6 @@ import {
   updateCirclePos,
   updateCircleVel,
 } from './shapes'
-import { createTimer } from './timer'
 
 const update = (step, { circle, bounds }) => {
   return {
@@ -20,162 +18,91 @@ const update = (step, { circle, bounds }) => {
   }
 }
 
-const gameMachine = Machine({
-  initial: 'idle',
-  states: {
-    idle: {
-      on: {
-        START: 'running',
-      },
-    },
-    running: {
-      on: {
-        PAUSE: 'paused',
-        STOP: 'stopped',
-      },
-      onEntry: 'startGame',
-    },
-    paused: {
-      on: {
-        RESUME: 'running',
-        STOP: 'stopped',
-      },
-      onEntry: 'pauseGame',
-    },
-    stopped: {
-      on: {
-        START: 'running',
-      },
-      onEntry: 'stopGame',
-    },
-  },
-})
-
 class Froke extends Component {
   constructor() {
     super()
 
-    const initialScene = {
-      circle: createCircle(20, Vec(320, 320), Vec(100, 50)),
-      bounds: createSquare(Vec(0, 0), Vec(640, 640)),
-    }
-
-    const getStartTimer = initialScene =>
-      createTimer({
-        step: 1 / 60,
-        update,
-        render: scene => this.setState({ scene }),
-        initialScene,
-      })
-
-    let unbindKeys
-
-    this.actionMap = {
-      startGame: () => {
-        this.stopTimer = this.startTimer()
-        unbindKeys = bindKeys(
-          window,
-          new Map([
-            [
-              ['w', 'ArrowUp'],
-              {
-                down: () => console.log('up-down'),
-                up: () => console.log('up-up'),
-              },
-            ],
-            [
-              ['d', 'ArrowRight'],
-              {
-                down: () => console.log('right-down'),
-                up: () => console.log('right-up'),
-              },
-            ],
-            [
-              ['s', 'ArrowDown'],
-              {
-                down: () => console.log('down-down'),
-                up: () => console.log('down-up'),
-              },
-            ],
-            [
-              ['a', 'ArrowLeft'],
-              {
-                down: () => console.log('left-down'),
-                up: () => console.log('left-up'),
-              },
-            ],
-          ])
-        )
+    this.engine = createEngine({
+      step: 1 / 60,
+      tick: update,
+      subscribe: ({ state, status }) =>
+        this.setState({ game: { state, status } }),
+      initialState: {
+        circle: createCircle(20, Vec(320, 320), Vec(100, 50)),
+        bounds: createSquare(Vec(0, 0), Vec(640, 640)),
       },
-      pauseGame: () => {
-        this.stopTimer()
-        this.startTimer = getStartTimer(this.state.scene)
+      keyBindings: {
+        element: window,
+        bindings: new Map([
+          [
+            ['w', 'ArrowUp'],
+            {
+              down: () => console.log('up-down'),
+              up: () => console.log('up-up'),
+            },
+          ],
+          [
+            ['d', 'ArrowRight'],
+            {
+              down: () => console.log('right-down'),
+              up: () => console.log('right-up'),
+            },
+          ],
+          [
+            ['s', 'ArrowDown'],
+            {
+              down: () => console.log('down-down'),
+              up: () => console.log('down-up'),
+            },
+          ],
+          [
+            ['a', 'ArrowLeft'],
+            {
+              down: () => console.log('left-down'),
+              up: () => console.log('left-up'),
+            },
+          ],
+        ]),
       },
-      stopGame: () => {
-        this.stopTimer()
-        this.setState({ scene: initialScene })
-        this.startTimer = getStartTimer(initialScene)
-        unbindKeys()
-      },
-    }
+    })
 
     this.state = {
-      game: gameMachine.initialState.value,
-      scene: initialScene,
+      game: {
+        state: this.engine.getState(),
+        status: this.engine.getStatus(),
+      },
     }
-
-    this.startTimer = getStartTimer(initialScene)
-  }
-
-  dispatch(event) {
-    const nextState = gameMachine.transition(this.state.game, event)
-
-    nextState.actions.forEach(actionKey => {
-      const action = this.actionMap[actionKey]
-
-      if (action) {
-        action(event)
-      }
-    })
-
-    this.setState({
-      game: nextState.value,
-    })
   }
 
   render() {
     return (
       <div>
         <p>
-          Current state: <b>{this.state.game}</b>
+          Current status: <b>{this.state.game.status}</b>
         </p>
-        {this.state.game === 'idle' || this.state.game === 'stopped' ? (
-          <button onClick={() => this.dispatch({ type: 'START' })}>
-            Start
-          </button>
+        {this.state.game.status === 'idle' ||
+        this.state.game.status === 'stopped' ? (
+          <button onClick={() => this.engine.start()}>Start</button>
         ) : (
           undefined
         )}
-        {this.state.game === 'running' ? (
-          <button onClick={() => this.dispatch({ type: 'PAUSE' })}>
-            Pause
-          </button>
+        {this.state.game.status === 'running' ? (
+          <button onClick={() => this.engine.pause()}>Pause</button>
         ) : (
           undefined
         )}
-        {this.state.game === 'paused' ? (
-          <button onClick={() => this.dispatch({ type: 'RESUME' })}>
-            Resume
-          </button>
+        {this.state.game.status === 'paused' ? (
+          <button onClick={() => this.engine.resume()}>Resume</button>
         ) : (
           undefined
         )}
-        {this.state.game === 'running' || this.state.game === 'paused' ? (
-          <button onClick={() => this.dispatch({ type: 'STOP' })}>Stop</button>
+        {this.state.game.status === 'running' ||
+        this.state.game.status === 'paused' ? (
+          <button onClick={() => this.engine.stop()}>Stop</button>
         ) : (
           undefined
         )}
-        <Canvas scene={this.state.scene} />
+        <Canvas scene={this.state.game.state} />
       </div>
     )
   }
