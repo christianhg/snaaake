b5ed593d1d1a1d1bb06fd2153e321018b9f1aace
Christian Hamburger Grøngaard <christianhg@gmail.com>
2021-11-13 13:29:17 +0100

    WIP

--- src/snaaake.tsx
+++ src/snaaake.tsx
@@ -1,71 +1,50 @@
 import 'modern-normalize';
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Canvas, CanvasSettings } from './engine/canvas';
-import {
-  SnakeMachine,
-  SnakeMachineState,
-  createSnakeMachine,
-} from './snake/snake-machine';
-import {
-  Apples,
-  Bounds,
-  Snake,
-  willExceedBounds,
-  willEatApple,
-  willHitItself,
-  moveSnake,
-  growSnake,
-  getInitialSnakeData,
-  addApple,
-} from './snake/snake';
-import { drawScene } from './snake/draw-snake';
 import { bindKeys } from './engine/keyboard';
 import { createTimer } from './engine/timer';
+import { drawScene } from './snake/draw-snake';
+import {
+  addApple,
+  Apples,
+  Bounds,
+  getInitialSnakeData,
+  growSnake,
+  moveSnake,
+  Snake,
+  willEatApple,
+  willExceedBounds,
+  willHitItself,
+} from './snake/snake';
+import { createSnakeMachine, SnakeMachineState } from './snake/snake-machine';
 
-export class Snaaake extends Component<
-  CanvasSettings,
-  {
-    game: {
-      apples: Apples;
-      bounds: Bounds;
-      snake: Snake;
-    };
-    status: SnakeMachineState<Apples, Bounds, Snake>;
-  }
-> {
-  private snakeMachine: SnakeMachine<Apples, Bounds, Snake>;
+export const Snaaake: React.FC<CanvasSettings> = ({ width, height, scale }) => {
+  const [status, setStatus] =
+    useState<SnakeMachineState<Apples, Bounds, Snake>>('idle');
+  const [gameState, setGameState] = useState(
+    getInitialSnakeData({ width, height })
+  );
 
-  constructor(props: CanvasSettings) {
-    super(props);
+  const snakeMachine = createSnakeMachine<Apples, Bounds, Snake>({
+    initialData: gameState,
+    resetData: () => getInitialSnakeData({ width, height }),
+    updateApples: addApple,
+    willExceedBounds,
+    willEatApple,
+    willHitItself,
+    moveSnake,
+    growSnake,
+    onUpdate: ({ apples, snake, state }) => {
+      setGameState({
+        ...gameState,
+        apples,
+        snake,
+      });
+      setStatus(state);
+    },
+  });
 
-    const game = getInitialSnakeData(props);
-
-    this.state = {
-      game,
-      status: 'idle',
-    };
-
-    this.snakeMachine = createSnakeMachine<Apples, Bounds, Snake>({
-      initialData: game,
-      resetData: () => getInitialSnakeData(props),
-      updateApples: addApple,
-      willExceedBounds,
-      willEatApple,
-      willHitItself,
-      moveSnake,
-      growSnake,
-      onUpdate: ({ apples, snake, state }) => {
-        this.setState({
-          game: {
-            ...this.state.game,
-            apples,
-            snake,
-          },
-          status: state,
-        });
-      },
-    });
-
+  useEffect(() => {
     bindKeys({
       element: window,
       handlers: new Map([
@@ -73,7 +52,7 @@ export class Snaaake extends Component<
           [' '],
           {
             down: () => {
-              this.snakeMachine.send('SPACE');
+              snakeMachine.send('SPACE');
             },
           },
         ],
@@ -81,7 +60,7 @@ export class Snaaake extends Component<
           ['Escape'],
           {
             down: () => {
-              this.snakeMachine.send('ESCAPE');
+              snakeMachine.send('ESCAPE');
             },
           },
         ],
@@ -89,7 +68,7 @@ export class Snaaake extends Component<
           ['w', 'ArrowUp'],
           {
             down: () => {
-              this.snakeMachine.send('UP');
+              snakeMachine.send('UP');
             },
           },
         ],
@@ -97,7 +76,7 @@ export class Snaaake extends Component<
           ['d', 'ArrowRight'],
           {
             down: () => {
-              this.snakeMachine.send('RIGHT');
+              snakeMachine.send('RIGHT');
             },
           },
         ],
@@ -105,7 +84,7 @@ export class Snaaake extends Component<
           ['s', 'ArrowDown'],
           {
             down: () => {
-              this.snakeMachine.send('DOWN');
+              snakeMachine.send('DOWN');
             },
           },
         ],
@@ -113,68 +92,64 @@ export class Snaaake extends Component<
           ['a', 'ArrowLeft'],
           {
             down: () => {
-              this.snakeMachine.send('LEFT');
+              snakeMachine.send('LEFT');
             },
           },
         ],
       ]),
     });
-  }
 
-  componentDidMount() {
     createTimer({
       step: 1 / 8,
       onTick: () => {
-        this.snakeMachine.send('TICK');
+        snakeMachine.send('TICK');
       },
     });
-  }
+  }, [snakeMachine]);
 
-  render() {
-    return (
-      <div>
-        <h1>🅂🄽🄰🄰🄰🄺🄴</h1>
-        <p className="score">{this.state.game.snake.length - 1}</p>
-        <Canvas
-          settings={this.props}
-          state={this.state.game}
-          draw={drawScene}
-        />
-        <p>
-          {this.state.status === 'idle' ? (
-            <>
-              <kbd>↑</kbd>
-              <kbd>→</kbd>
-              <kbd>↓</kbd>
-              <kbd>←</kbd>
-            </>
-          ) : this.state.status === 'moving' ? (
-            <>
-              <kbd>
-                Space <span>(pause)</span>
-              </kbd>
-            </>
-          ) : this.state.status === 'paused' ? (
-            <>
-              <kbd>
-                Space <span>(resume)</span>
-              </kbd>
-              <kbd>
-                Escape <span>(restart)</span>
-              </kbd>
-            </>
-          ) : (
-            <>
-              <kbd>
-                Space <span>(restart)</span>
-              </kbd>
-            </>
-          )}
-        </p>
-        <p className="src">
-          <a href="https://github.com/christianhg/snaaake">{'{src}'}</a>
-        </p>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>🅂🄽🄰🄰🄰🄺🄴</h1>
+      <p className="score">{gameState.snake.length - 1}</p>
+      <Canvas
+        settings={{ width, height, scale }}
+        state={gameState}
+        draw={drawScene}
+      />
+      <p>
+        {status === 'idle' ? (
+          <>
+            <kbd>↑</kbd>
+            <kbd>→</kbd>
+            <kbd>↓</kbd>
+            <kbd>←</kbd>
+          </>
+        ) : status === 'moving' ? (
+          <>
+            <kbd>
+              Space <span>(pause)</span>
+            </kbd>
+          </>
+        ) : status === 'paused' ? (
+          <>
+            <kbd>
+              Space <span>(resume)</span>
+            </kbd>
+            <kbd>
+              Escape <span>(restart)</span>
+            </kbd>
+          </>
+        ) : (
+          <>
+            <kbd>
+              Space <span>(restart)</span>
+            </kbd>
+          </>
+        )}
+      </p>
+      <p className="src">
+        <a href="https://github.com/christianhg/snaaake">{'{src}'}</a>
+      </p>
+    </div>
+  );
+};
